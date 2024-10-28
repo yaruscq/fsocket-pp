332b6f45facd6fb80b61b650e86d8a7e5ca94cf6
yaruscq <cq1.chou@gmail.com>
2024-10-29 02:15:26 +0800

    test environ vars

--- app.py
+++ app.py
@@ -9,13 +9,17 @@ from chatapp import create_app, socketio
 # RENDER PLATFORM 1
 # @ Render: 
 # Start Command: $ gunicorn --worker-class eventlet -w 1 app:app
+# port=5000, 5233 成功！
+# port=5233， 成功！ 但在 Render Platform 上顯示：Detected service running on port 5000 仍然是 5000
 
 app = create_app()
-port = int(os.environ.get('PORT', 5233))
+# port = int(os.environ.get('PORT', 5000))
+port = int(os.getenv('PORT', 5000))
 socketio.run(app, host="0.0.0.0", port=port)
 
 
-# RENDER PLATFORM 2
+
+# RENDER PLATFORM 2 (不成功！)
 # @ Render: 
 # Start Command: $ gunicorn --worker-class eventlet -w 1 app:app "--bind 0.0.0.0:$PORT"
 # Try define environment var $PORT; Try remove "--bind 0.0.0.0:$PORT": both doesn't work!
--- chatapp/routes.py
+++ chatapp/routes.py
@@ -1,7 +1,14 @@
+import os
 from flask import Blueprint, render_template
+
+
 
 main = Blueprint("main", __name__)
 
+var_val = os.getenv('var_val')
+name1 = os.getenv('name1')
+name2 = os.getenv('name2')
+
 @main.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", var_val=var_val, name1=name1, name2=name2)
