8e3b38a6fb1ef4bee218069d16600c1f4ab62356
yaruscq <cq1.chou@gmail.com>
2024-10-29 00:32:39 +0800

    Try to switch back to : port = int(os.environ.get('PORT', 5000))

--- app.py
+++ app.py
@@ -10,13 +10,15 @@ from chatapp import create_app, socketio
 # @ Render: 
 # Start Command: $ gunicorn --worker-class eventlet -w 1 app:app
 
-# app = create_app()
-# port = int(os.environ.get('PORT', 5000))
-# socketio.run(app, host="0.0.0.0", port=port)
+app = create_app()
+port = int(os.environ.get('PORT', 5000))
+socketio.run(app, host="0.0.0.0", port=port)
 
 
 # RENDER PLATFORM 2
 # @ Render: 
-# Start Command: $ gunicorn --worker-class eventlet -w 1 app:app --bind 0.0.0.0:$PORT
-app = create_app()
-socketio.run(app)
+# Start Command: $ gunicorn --worker-class eventlet -w 1 app:app "--bind 0.0.0.0:$PORT"
+# Try define environment var $PORT; Try remove "--bind 0.0.0.0:$PORT": both doesn't work!
+
+# app = create_app()
+# socketio.run(app)
